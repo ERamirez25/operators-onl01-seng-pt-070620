3988f4763d850b016464e78d17d60936e5a9cb85
Elaine Ramirez <elaineramirez208@yahoo.com>
2020-07-11 21:53:45 +0000

    Automatically backed up by Learn

--- lib/operations.rb
+++ lib/operations.rb
@@ -14,13 +14,8 @@ end
 
 
 def not_safe?(speed)
-  if speed > 60
-    true
-  elsif speed < 40
-   true 
- else speed == 40 
-   false
-    
+  if speed > 60 || speed < 40 ? true : false 
+  
  
   end 
 	
